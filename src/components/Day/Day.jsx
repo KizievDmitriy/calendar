98f2455cfb3412570d489dcdde21d30ea75783c0
KizievDmitriy <kizievdmitrii@gmail.com>
2022-09-07 13:03:18 +0300

    add styles header

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import dayjs from "dayjs";
 import s from './day.module.scss';
-export default function Day({day, rowIdx}) {
+
+
+export default function Day({ day, rowIdx }) {
+    
   return (
     <div className={s.container}>
       <div>
         {rowIdx === 0 && (<p className={s.day}>{day.format('ddd')}</p>)}
-        <p className={s.number}>{day.format('DD')}</p>
+        {day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ?  //currentDay
+          (<p className={s.currentDay}>{day.format('DD')}</p>) :
+          (<p className={s.number}>{day.format('DD')}</p>)}
       </div>    
     </div>
   )
